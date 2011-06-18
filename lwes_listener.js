3531d4980eda7fb745ef79e43fcb383294b3b405
Salvador Fuentes Jr <sfuentes@attinteractive.com>
2011-06-17 21:19:11 -0700

    refactor to use/export an LWES class

--- lwes_listener.js
+++ lwes_listener.js
@@ -1,17 +1,9 @@
 #!/usr/bin/env node
 
-var Buffer = require('buffer').Buffer;
-var parse = require('lwes').parse;
-var fs = require('fs');
-var dgram = require('dgram');
-var logfile = fs.createWriteStream('lwes.bin');
+var LWES = require('./lib/lwes').LWES;
+var lwesObj = new LWES();
+var filteredEvents = ['EventTypeA', 'EventTypeB']
 
-dgram.createSocket("udp4").on('message', function(message, info){
-  var buffer = new Buffer(message);
-  var lwesEvent = parse(buffer);
-  console.log(buffer);
-  console.log(lwesEvent);
-}).on('listening', function(){
-  console.log("start listening on 0.0.0.0:12345.");
-}).bind(12345);
-
+lwesObj.createListener(filteredEvents, function(lwesEvent) {
+  console.log("I Found a filtered event: " + lwesEvent.type);
+});
