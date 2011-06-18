90b5e1544d9b46be689c6c97c836e884259115ce
Salvador Fuentes Jr <sfuentes@attinteractive.com>
2011-06-18 05:31:34 -0700

    missing semicolon on filtered list

--- lwes_listener.js
+++ lwes_listener.js
@@ -2,7 +2,7 @@
 
 var LWES = require('./lib/lwes').LWES;
 var lwesObj = new LWES();
-var filteredEvents = ['EventTypeA', 'EventTypeB']
+var filteredEvents = ['EventTypeA', 'EventTypeB'];
 
 lwesObj.createListener(filteredEvents, function(lwesEvent) {
   console.log("I Found a filtered event: " + lwesEvent.type);
